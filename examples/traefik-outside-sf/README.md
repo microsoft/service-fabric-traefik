ca61da45647dd16a6a4ab552044e742fb25f5443
Michael <mmatur@users.noreply.github.com>
2024-04-01 23:34:25 +0200

    feat: add example for traefik running outside of sf (#32)
    * feat: add example for traefik running outside sf
    
    * review

---
+++ examples/traefik-outside-sf/README.md
@@ -0,0 +1,65 @@
+# Traefik outside Service Fabric on Linux
+
+This example shows how to run Traefik Proxy on a virtual machine, with docker-compose, to expose and manage traffic of Service Fabric applications.
+
+## Pre-requisites
+
+You'll need sfctl, see [here](https://learn.microsoft.com/en-us/azure/service-fabric/service-fabric-get-started-linux?tabs=sdksetupubuntu%2Clocalclusteroneboxcontainer#script-installation)
+
+/!\ Do not install latest version of sfctl /!\
+
+```shell
+pipx install sfctl==10.0.0
+```
+
+## Deploy Service Fabric and virtual machine
+
+You'll need to get your Subscription ID, see [here](https://portal.azure.com/#view/Microsoft_Azure_Billing/SubscriptionsBladeV2) and select a strong password.
+
+```shell
+export AZURE_SUBSCRIPTION="XXX"
+export SF_PASSWORD="ChangemeIAMinsecureTraefik!2024"
+./deploy-sf.sh
+```
+
+It will deploy both Service Fabric cluster and a single vm running Traefik and Service Fabric Discovery Service with docker compose.
+
+## Connect sfctl
+
+```shell
+sfctl cluster select --no-verify --pem traefik-clustereastuscloudappazurecom.pem --endpoint https://traefik-cluster.eastus.cloudapp.azure.com:19080
+```
+
+## Deploy Applications
+
+```shell
+./deploy-whoami.sh
+```
+
+![Cluster SF state](assets/cluster-sf.png "Cluster SF state")
+
+![Whoami application](assets/whoami-application.png "Whoami application")
+
+## Access to VMs
+
+![NAT SSH](assets/natssh.png "NAT SSH")
+
+You can access to VMs with ssh using NAT inbound rules.
+
+In this example:
+
+```shell
+ssh traefik@52.226.132.159 -p 3389
+```
+
+## Clean
+
+```shell
+clean-whoami.sh
+clean-traefik.sh
+clean-sf.sh
+```
+
+## Links
+
+* [Official linux tutorial](https://learn.microsoft.com/en-us/azure/service-fabric/service-fabric-quickstart-containers-linux)
---
+++ examples/traefik-outside-sf/deploy-sf.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Variables
+ResourceGroupName='traefik-cluster'
+ClusterName='traefik-cluster'
+Location='eastus'
+Password="${SF_PASSWORD}"
+Subject='traefik-cluster.eastus.cloudapp.azure.com'
+VaultName='traefik-cluster'
+VmPassword="${SF_PASSWORD}"
+VmUserName='traefik'
+
+# Login to Azure and set the subscription
+az login
+
+az account set --subscription $AZURE_SUBSCRIPTION
+
+# Create resource group
+az group create --name $ResourceGroupName --location $Location
+
+# Create secure five node Linux cluster. Creates a key vault in a resource group
+# and creates a certificate in the key vault. The certificate's subject name must match
+# the domain that you use to access the Service Fabric cluster.  The certificate is downloaded locally.
+az sf cluster create --resource-group $ResourceGroupName --location $Location --certificate-output-folder . --certificate-password $Password --certificate-subject-name $Subject --cluster-name $ClusterName --cluster-size 5 --os UbuntuServer1604 --vault-name $VaultName --vault-rg $ResourceGroupName --vm-password $VmPassword --vm-user-name $VmUserName
+
+# Create traefik VM
+az vm create \
+    --name traefik \
+    --resource-group $ResourceGroupName \
+    --size Standard_B2s \
+    --image Ubuntu2204 \
+    --admin-username $VmUserName --admin-password $VmPassword --vnet-name=VNet --subnet=Subnet-0 --custom-data ./vm-custom-data.sh
+
+# Open port for traefik VM
+az vm open-port --port 80 --resource-group $ResourceGroupName --name traefik --priority 900
+az vm open-port --port 8080 --resource-group $ResourceGroupName --name traefik --priority 901
+
+# Generate cert and key for traefik
+openssl pkcs12 -info -in traefik-clustereastuscloudappazurecom.pfx  -nodes -nocerts -out ./traefik/compose/certs/sf.key
+openssl pkcs12 -in traefik-clustereastuscloudappazurecom.pfx -clcerts -nokeys -out ./traefik/compose/certs/sf.crt
